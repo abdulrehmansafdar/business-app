41c0f4f1dedbc34c0a5ebe150a7e0f459e57ab41
Abdul Rehman <145101831+abdulrehmansafdar@users.noreply.github.com>
2024-02-08 03:07:06 +0500

    updated

---
+++ business_application_1/owner.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace signl_ogin
+{
+     class owner
+    {
+        public string owner_product;
+        public int owner_quantity;
+        public float owner_price;
+        public owner(string owner_product, int owner_quantity, float owner_price)
+        {
+            this.owner_product = owner_product;
+            this.owner_quantity = owner_quantity;
+            this.owner_price = owner_price;
+        }
+        public void ChangePrice(float newPrice)
+        {
+            owner_price = newPrice;
+            Console.WriteLine($"Price for {owner_product} has been changed to {newPrice}.");
+        }
+        public void GiveDiscount(float discountPercentage)
+        {
+            float discountAmount = owner_price * (discountPercentage / 100);
+            owner_price -= discountAmount;
+
+            Console.WriteLine($"Discount of {discountPercentage}% applied to {owner_product}. New price: {owner_price}");
+        }
+    }
+}
