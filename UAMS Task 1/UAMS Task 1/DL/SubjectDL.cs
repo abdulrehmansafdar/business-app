00547e7340812d4308ae764c096444a6cf2e6d3f
Abdul Rehman <145101831+abdulrehmansafdar@users.noreply.github.com>
2024-02-27 09:44:11 +0500

    Uams

---
+++ UAMS Task 1/UAMS Task 1/DL/SubjectDL.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace UAMS
+{
+    internal class SubjectDL
+    {
+        List<SubjectBL> AllSubjects = new List<SubjectBL>();
+        public static void StoreSubjectsInFile(string path, SubjectBL d)
+        {
+            StreamWriter file = new StreamWriter(path, true);
+            file.WriteLine(d.Code + "," + d.Type + "," + d.CreditHours + "," + d.SubjectFees);
+            file.Flush();
+            file.Close();
+        }
+    }
+}
